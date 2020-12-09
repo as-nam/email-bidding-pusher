681e809a378386f285d0df0f8a3c087e24bc59cb
as-nam <nam.tran@audienceserv.com>
2020-12-09 09:56:22 +0700

    Add types for recipient

--- index.ts
+++ index.ts
@@ -1,138 +1,160 @@
-import fs from "fs";
-import axios from "axios";
-import moment from "moment";
-import readline from "readline";
+import fs from 'fs';
+import axios from 'axios';
+import moment from 'moment';
+import readline from 'readline';
+import { Recipient } from './recipient.type';
 
-const url =
-    "https://api.emailbidding.com/api/p/publishers/172/lists/de_eb_as/recipients-batch?key=5d07251f58ae4e2cc6578d6333ede432&secret=77b9fa8e8211b4613619a015c81b984c8e52e220";
+const LIST = {
+  DE_AS: 'DE_EB_AS',
+  DE_MC: 'DE_MC',
+  UK: 'UK_1220',
+  FR_MC: 'FR_MC'
+};
+const url = `https://api.emailbidding.com/api/p/publishers/170/lists/fr_mc/recipients-batch?key=ca318982a21f29ce6777ac28df564109&secret=df762df7ce7709fc0e61c895fd2e2fca4fcd1d89`;
 
 let labels = [];
-let recipients = [];
+let recipients: Recipient[] = [];
 let total = 0;
 
-function mapArrayToObject(array) {
-    const object = {};
-    array.forEach((value, index) => {
-        object[labels[index]] = value;
-    });
-    return object;
+function mapArrayToObject(array: string[]): Recipient {
+  const object = {};
+  array.forEach((value, index) => {
+    object[labels[index]] = value;
+  });
+
+  return object as Recipient;
 }
 
-function formatDatetime(datetime) {
-    try {
-        if (datetime && moment(datetime, "YYYY-MM-DD").isValid()) {
-            return datetime ? moment(datetime).format("YYYY-MM-DD") : "";
+function formatRecipientList() {
+  recipients.forEach((recipient) => {
+    recipient.country = recipient.country;
+    recipient.gender = formatGender(recipient.gender);
+    recipient.last_activity_date = formatLastActivityDate(
+      recipient.last_activity_date
+    );
+    recipient.birthdate = formatBirthDate(recipient.birthdate);
+  });
+}
+
+function formatBirthDate(timestamp: string): string {
+  try {
+    if (timestamp) {
+      if (moment(new Date(timestamp)).isValid()) {
+        if (moment(new Date(timestamp)).isValid()) {
+          return moment(timestamp).format('YYYY-MM-DD');
         }
-        return "";
-    } catch (e) {
-        return "";
+      }
+      return '';
     }
+    return '';
+  } catch (e) {
+    return '';
+  }
 }
 
-function formatGender(gender) {
-    if (gender === "M" || gender === "F") {
-        return gender;
+function formatLastActivityDate(timestamp: string): string {
+  try {
+    if (timestamp) {
+      if (moment(new Date(timestamp)).isValid()) {
+        return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+      }
+      return '';
     }
-    return "NA";
+    return '';
+  } catch (e) {
+    return '';
+  }
 }
 
-function formatZipCode(zip) {
-    zip = zip.split(".")[0];
-    if (zip && zip.length <= 10) {
-        return zip;
-    }
-    return "";
+function formatGender(gender: string): string {
+  if (gender === 'M' || gender === 'F') {
+    return gender;
+  }
+  return 'NA';
 }
 
-function parsingRecipients(recipients) {
-    const array = [];
-    recipients.forEach((recipient) => {
-        array.push({
-            email_address: recipient.email,
-            country: "DE",
-            first_name: recipient.first_name || "",
-            last_name: recipient.last_name || "",
-            external_id: recipient.galaxy_id,
-            gender: formatGender(recipient.gender),
-            zipcode: formatZipCode(recipient.zip),
-            birthdate: formatDatetime(recipient.dob),
-        });
+const pushRecipientToEmailBidding = async () => {
+  try {
+    const response = await axios({
+      url,
+      method: 'POST',
+      data: {
+        subscribe: {
+          recipients
+        }
+      }
     });
-    return array;
-}
+    fs.writeFileSync(
+      `reports/${LIST.FR_MC}/success_${total}.json`,
+      JSON.stringify(response.data),
+      'utf8'
+    );
+  } catch (e) {
+    if (e.response && e.response.status === 400) {
+      fs.writeFileSync(
+        `reports/${LIST.FR_MC}/error_${total}.json`,
+        JSON.stringify(e.response.data.errors.children.recipients),
+        'utf8'
+      );
+    } else {
+      fs.writeFileSync(
+        `reports/${LIST.FR_MC}/failed_${total}.json`,
+        JSON.stringify(e),
+        'utf8'
+      );
+    }
+  }
+  total += recipients.length;
+  console.log(total);
+};
+
+let isHeader = false;
 
 (async () => {
-    const fileStream = fs.createReadStream("20200701_compiled_main_csa_2m.csv");
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity,
-    });
-    let i = 0;
-    for await (const line of rl) {
-        if (i === 0) {
-            labels = line.split(",");
-        } else {
-            recipients.push(mapArrayToObject(line.split(",")));
-        }
 
-        if (i === 200) {
-            const rows = parsingRecipients(recipients);
-            const data = {
-                subscribe: {
-                    recipients: rows,
-                },
-            };
-            try {
-                const response = await axios({
-                    url,
-                    method: "POST",
-                    data,
-                });
-                fs.writeFileSync(
-                    `reports/success_${total}.json`,
-                    JSON.stringify(response.data),
-                    "utf8"
-                );
-                total += 200;
-                console.log(total);
-            } catch (e) {
-                if (e.response && e.response.status === 400) {
-                    fs.writeFileSync(
-                        `reports/error_${total}.json`,
-                        JSON.stringify(
-                            e.response.data.errors.children.recipients
-                        ),
-                        "utf8"
-                    );
-                } else {
-                    console.log(e);
-                    fs.writeFileSync(
-                        `reports/failed_${total}.json`,
-                        JSON.stringify(e),
-                        "utf8"
-                    );
-                }
-            }
+  console.log(url);
 
-            i = 1;
-            recipients = [];
-        }
-        i++;
+
+  // if (!fs.existsSync(`reports/${LIST.DE_AS}`)) {
+  //   fs.mkdirSync(`reports/${LIST.DE_AS}`);
+  // }
+
+  // if (!fs.existsSync(`reports/${LIST.DE_MC}`)) {
+  //   fs.mkdirSync(`reports/${LIST.FR_MC}`);
+  // }
+
+  // if (!fs.existsSync(`reports/${LIST.UK}`)) {
+  //   fs.mkdirSync(`reports/${LIST.UK}`);
+  // }
+
+  // if (!fs.existsSync(`./reports/${LIST.FR_MC}`)) {
+  //   fs.mkdirSync(`reports/${LIST.FR_MC}`);
+  // }
+
+
+  const fileStream = fs.createReadStream(
+    './DE_UK_FR_Emailbidding/UK_Emailbidding_963k.csv'
+  );
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  let i = 0;
+  for await (const line of rl) {
+    if (!isHeader) {
+      labels = line.split(',');
+      isHeader = true;
+    } else {
+      formatRecipientList();
+      recipients.push(mapArrayToObject(line.split(',')));
+      i++;
     }
-    const rows = parsingRecipients(recipients);
-    const response = await axios({
-        url,
-        method: "POST",
-        data: {
-            subscribe: {
-                recipients: rows,
-            },
-        },
-    });
-    console.log(total + recipients.length);
-    fs.writeFileSync(
-        `reports/success_${total}.json`,
-        JSON.stringify(response.data),
-        "utf8"
-    );
+
+    if (i === 200) {
+      i = 0;
+      await pushRecipientToEmailBidding();
+      recipients = [];
+    }
+  }
+  await pushRecipientToEmailBidding();
 })();
---
+++ recipient.type.ts
@@ -0,0 +1,27 @@
+export type Recipient  = {
+  email_address: string;
+  country: string;
+  language?: string;
+  ip_address?: string;
+  subscription_source?: string;
+  subscription_date?: string;
+  gender?: string;
+  title?: string;
+  first_name?: string;
+  last_name?: string;
+  birthdate?: string;
+  address?: string;
+  zipcode?: string;
+  phone1?: string;
+  phone2?: string;
+  last_activity_date?: string;
+  subscription_status?: string;
+  unsubscription_ip?: string;
+  unsubscription_date?: string;
+  complaint?: string;
+  complaint_date?: string;
+  bounce?: string;
+  bounce_date?: string;
+  category_ids?: string;
+  external_id?: string;
+}
